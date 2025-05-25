8acd452989be483103f28107a7789001d0bac3b8
Shrenik Jain <shrenikkjain81@gmail.com>
2025-05-24 19:42:56 -0700

    new train

--- run.sh
+++ run.sh
@@ -7,6 +7,17 @@ source /src/code/smarts.sh
 cd /src/code/
 source source_installations.sh
 
+nvidia-smi
+
+echo "**************************************************************************"
+echo "Starting training with SMARTS..."
+echo "**************************************************************************"
+
 python train.py --use_exploration --use_interaction
+
+echo "**************************************************************************"
+echo "Training completed successfully."
+echo "**************************************************************************"
+ls /src/code
 
 zip /src/results/training_log.zip -r /src/code/training_log/
